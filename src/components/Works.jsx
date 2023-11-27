8454f13c4ce10b3bce97119377dc67cce64d5680
zesuma <trevercumming@icloud.com>
2023-11-27 18:40:46 -0500

    update dbt image

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -68,7 +68,7 @@ const Works = () => {
           variants={fadeIn("", "", 0.1, 1)}
           className="mt-3 text-secondary text-[1.1rem] max-w-3xl leading-[1.8rem]"
         >
-        Following projects showcase my skills and experience through real-world examples of my work. Each project is briefly described with links to the website. It reflects my ability to solve complex problems, work with different technologies, and manage projects effectively.
+        The following projects showcase my skills and experience through real-world examples of my work. Each project is briefly described with links to the website. It reflects my ability to solve complex problems, work with different technologies, and manage projects effectively.
         </motion.p>
       </div>
       <div className="mt-20 flex flex-wrap gap-7">
